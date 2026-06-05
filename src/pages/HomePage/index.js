19a23cf2feebbcf13d597376d164aab52b7187e8
dinesh <dineshmanikanta016@gmail.com>
2026-06-05 11:55:47 +0530

    navigate is established in home page if jwt_token not exists it will navigates to the login page

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,6 +1,17 @@
 import './index.css'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
 const HomePage = () => {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const jwt_token = localStorage.getItem('jwt_token')
+        if (jwt_token === null) {
+            return navigate('/login')
+        }
+    })
+    
     return (
         <div className="home-page-main-container">
             <h1>This is Home Page!</h1>
